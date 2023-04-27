5ad8a92e616eef2050716a043eaa085d049439a8
Wolfgang Reichardt <wolfgang@gentlymad.org>
2023-04-27 16:08:41 +0200

    - Improved GamestateService.SwitchTo<> summary so it is clear, that this happens in an async fashion

--- Assets/Framework/Services/GamestateService/IGamestateService.cs
+++ Assets/Framework/Services/GamestateService/IGamestateService.cs
@@ -10,10 +10,10 @@ namespace Framework.Services.GamestateService {
 		/// </summary> 
 		void Register(IGamestate gamestate);
 
-        /// <summary> 
-        ///Switch to a gamestate
-        /// </summary> 
-        void SwitchTo<TGamestate>(object context = null) where TGamestate : IGamestate;
+		/// <summary> 
+		///Switch to a gamestate. This does NOT happen immediatly and might happen in the next frame due to the async nature of gamestates! If you want to setup the gamestate, pass it a context and then setup the gamestate via gamestate.OnEnterAsync/.OnPreEnter/.OnPostEnter
+		/// </summary> 
+		void SwitchTo<TGamestate>(object context = null) where TGamestate : IGamestate;
 
         Task TickAsync(float deltaTime);
     }
